6c3d97430c072ce87cc2de4f31eb33937d0523c1
Justin Yi <justinjyi96@gmail.com>
2023-01-17 14:13:27 -0500

    adjusting handlebar #ifs in main.handlebar

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const createacount = require('./createacount');
 // const projectRoutes = require('./projectRoutes');
 
-router.use('/createacount', createacount;
+router.use('/createacount', createacount);
 // router.use('/projects', projectRoutes);
 
 module.exports = router;

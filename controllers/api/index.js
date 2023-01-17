6499fc5159fe71c855827d1697d06ca31b0d8549
Justin Yi <justinjyi96@gmail.com>
2023-01-17 14:21:11 -0500

    added a GET request to home-page, dynamically rendering to homepage.handlebar template

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const createacount = require('./createacount');
-// const projectRoutes = require('./projectRoutes');
-
-router.use('/createacount', createacount);
-// router.use('/projects', projectRoutes);
-
-module.exports = router;

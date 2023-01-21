6125a9a253d81ac5548c9c66244e7cdbc9b4caa5
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-21 15:35:12 -0500

    route debugging

--- controllers/index.js
+++ controllers/index.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
-<<<<<<< HEAD
-const dashboardRoutes = require('./dashboardRoutes');
-=======
 const profileRoutes = require('./profile-routes');
-
->>>>>>> 62803ad6208b02cb978025938894a25e9777b71b
 
 router.use('/', homeRoutes);
 
 router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;

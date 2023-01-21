b51aab20011a13dda9708aab179caac274b08d04
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-21 14:56:31 -0500

    searched routes in progress

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const jobRoutes = require('./job-routes');
 
 router.use('/users', userRoutes);
+router.use('/jobs', jobRoutes);
 
 module.exports = router;

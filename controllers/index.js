c2186c651e4be4cfbb201e4fffd0a715e26973ea
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-21 14:21:49 -0500

    dashboard and comment routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
